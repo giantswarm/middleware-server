2824a97a669a5852830ca07c54d90544554a362b
Tim Schindler <tim@catalyst-zero.com>
2014-05-28 11:39:08 +0000

    fixed examples in gitignore

---
+++ example/middleware/middleware.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	srvPkg "github.com/catalyst-zero/middleware-server"
+	logPkg "github.com/op/go-logging"
+)
+
+type V1 struct {
+	Logger *logPkg.Logger
+}
+
+func (this *V1) middlewareOne(res http.ResponseWriter, req *http.Request, ctx *srvPkg.Context) error {
+	this.Logger.Debug("middleware one")
+	return ctx.Next()
+}
+
+func (this *V1) middlewareTwo(res http.ResponseWriter, req *http.Request, ctx *srvPkg.Context) error {
+	this.Logger.Debug("middleware two")
+	return ctx.Response.PlainText("hello world", http.StatusOK)
+}
+
+func main() {
+	logger := srvPkg.NewSimpleLogger("not-found-example")
+	v1 := &V1{Logger: logger}
+
+	srv := srvPkg.NewServer("127.0.0.1", "8080")
+	srv.SetLogger(logger)
+
+	srv.Serve("GET", "/v1/hello-world", v1.middlewareOne, v1.middlewareTwo)
+
+	srv.Listen()
+}
