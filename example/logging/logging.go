7154c5736cd818726b62e35539c9bda44a6c1586
Tim Schindler <tim@catalyst-zero.com>
2014-05-30 08:16:53 +0000

    better example

--- example/logging/logging.go
+++ example/logging/logging.go
@@ -12,12 +12,12 @@ type V1 struct {
 }
 
 func (this *V1) middlewareOne(res http.ResponseWriter, req *http.Request, ctx *srvPkg.Context) error {
-	this.Logger.Critical("middleware one")
-	this.Logger.Error("middleware one")
-	this.Logger.Warning("middleware one")
-	this.Logger.Notice("middleware one")
-	this.Logger.Info("middleware one")
-	this.Logger.Debug("middleware one")
+	this.Logger.Critical("middleware %s", "one")
+	this.Logger.Error("middleware %s", "one")
+	this.Logger.Warning("middleware %s", "one")
+	this.Logger.Notice("middleware %s", "one")
+	this.Logger.Info("middleware %s", "one")
+	this.Logger.Debug("middleware %s", "one")
 
 	return ctx.Response.PlainText("hello world", http.StatusOK)
 }
