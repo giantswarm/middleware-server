2af99b5426cff50a024eae241fc09723b05fa6e8
Dennis Benkert <spinecrasher@googlemail.com>
2014-10-24 16:29:58 +0200

    added welcome middleware constructor

--- example/welcome/welcome.go
+++ example/welcome/welcome.go
@@ -1,28 +1,12 @@
 package main
 
-import (
-	"net/http"
-
-	srvPkg "github.com/catalyst-zero/middleware-server"
-	logPkg "github.com/op/go-logging"
-)
-
-type V1 struct {
-	Logger *logPkg.Logger
-}
-
-func (this *V1) welcomeMiddleware(res http.ResponseWriter, req *http.Request, ctx *srvPkg.Context) error {
-	return ctx.Response.PlainText("This is welcome example 1.1.0", http.StatusOK)
-}
+import srvPkg "github.com/catalyst-zero/middleware-server"
 
 func main() {
-	logger := srvPkg.NewLogger(srvPkg.LoggerOptions{Name: "middleware-example"})
-	v1 := &V1{Logger: logger}
-
 	srv := srvPkg.NewServer("127.0.0.1", "8080")
 	srv.SetLogger(logger)
 
-	srv.Serve("GET", "/", v1.welcomeMiddleware)
+	srv.Serve("GET", "/", srvPkg.NewWelcomeMiddleware("welcome example", "0.0.1"))
 
 	srv.Listen()
 }
