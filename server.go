578e704c270858e6b590bf4e969c4717acfededd
Tim Schindler <tim@catalyst-zero.com>
2014-11-03 15:24:23 +0000

    add newline to welcome middleware

--- server.go
+++ server.go
@@ -18,7 +18,7 @@ type Middleware func(http.ResponseWriter, *http.Request, *Context) error
 
 func NewWelcomeMiddleware(appName, version string) Middleware {
 	return func(res http.ResponseWriter, rep *http.Request, ctx *Context) error {
-		return ctx.Response.PlainText(fmt.Sprintf("This is %s version %s", appName, version), http.StatusOK)
+		return ctx.Response.PlainText(fmt.Sprintf("This is %s version %s\n", appName, version), http.StatusOK)
 	}
 }
 
