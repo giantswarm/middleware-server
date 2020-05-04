c68b5ab6019bf9851a3779e9ad30e7f70ea48de3
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-05-04 12:55:46 +0300

    Bring back logger

---
+++ logger.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"os"
+
+	"github.com/juju/errgo"
+
+	Stdlog "log"
+
+	log "github.com/op/go-logging"
+)
+
+type LoggerOptions struct {
+	Name  string
+	Level string
+}
+
+// NewSimpleLogger creates a new logger with a default backend logging to `os.Stderr`.
+func NewLogger(options LoggerOptions) *log.Logger {
+	// Create new logger.
+	logger := log.MustGetLogger(options.Name)
+
+	// Configure logger.
+	log.SetFormatter(log.MustStringFormatter("[%{level}] %{message}"))
+	logBackend := log.NewLogBackend(os.Stderr, "", Stdlog.LstdFlags|Stdlog.Lshortfile)
+	logBackend.Color = true
+	log.SetBackend(logBackend)
+
+	// Set log level.
+	if options.Level != "" {
+		logLevel, err := log.LogLevel(options.Level)
+		if err != nil {
+			panic(errgo.Mask(err))
+		}
+
+		log.SetLevel(logLevel, options.Name)
+	}
+
+	return logger
+}
